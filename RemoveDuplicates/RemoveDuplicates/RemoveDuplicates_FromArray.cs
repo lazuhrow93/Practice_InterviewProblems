4b579ca9caf941c987863c1269356c4261a82f19
Lazaro Hernandez (A) <lhernandez93@outlook.com>
2019-09-19 23:50:41 -0500

    added code to find length of unique array

--- RemoveDuplicates/RemoveDuplicates/RemoveDuplicates_FromArray.cs
+++ RemoveDuplicates/RemoveDuplicates/RemoveDuplicates_FromArray.cs
@@ -7,7 +7,28 @@ namespace RemoveDuplicates
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            int[] list_of_nums2 = new int[3] { 1, 1, 2 };
+            int[] list_of_nums = new int[10] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+
+            int unique_length = 1;
+            bool changed = false;
+            int curr_num = list_of_nums[0];
+
+
+            for (int i = 1; i < list_of_nums.Length; ++i)
+            {
+                if(list_of_nums[i] != curr_num)
+                {
+                    ++unique_length;
+                    curr_num = list_of_nums[i];
+                }
+            }
+
+
+            Console.WriteLine("LENGTH: " + unique_length);
             Console.ReadKey();
+
         }
     }
 }
